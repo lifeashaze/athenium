2c341ebb6cfec2d6416721004478dd31e5e0cdc2
Keyur Kulkarni <163241275+Keyur-Hat@users.noreply.github.com>
2024-11-07 14:24:26 +0530

    Update Header.tsx
    Landing ui overhaul v0.1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,14 +10,30 @@ export function Header() {
     router.push('/sign-in');
   };
 
+  const handleSignUp = () => {
+    router.push('/sign-up');
+  };
+
   return (
     <header>
       <div className="container flex h-16 items-center justify-between px-4 md:px-6">
         <div className="flex items-center gap-4">
           <Link href="/" className="text-2xl font-bold">
-            Athenium
+            athenium
           </Link>
         </div>
+        <div className='space-x-7'>
+        <Link href="" className="text-2xl">
+            about
+          </Link>
+          <Link href="" className="text-2xl">
+            changelog
+          </Link>
+          <Link href="" className="text-2xl">
+            docs
+          </Link>
+        </div>
+        <div className='space-x-3'>
         <SignedOut>
           <button
             onClick={handleSignIn}
@@ -25,8 +41,16 @@ export function Header() {
           >
             Sign In
           </button>
+          <button 
+            onClick={handleSignUp}
+            className='bg-gray-900 text-white px-4 py-2 rounded-md text-sm font-medium transition-colors hover:bg-gray-800 focus:bg-gray-800 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-200 dark:focus:bg-gray-200'
+           >
+            Get Started
+           </button>
         </SignedOut>
-        <SignedIn>
+        </div>
+
+        <SignedIn> 
           <div className='flex gap-5'>
           <Link href="/dashboard"
             className="bg-gray-900 text-white px-4 py-2 rounded-md text-sm font-medium transition-colors hover:bg-gray-800 focus:bg-gray-800 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-200 dark:focus:bg-gray-200"
